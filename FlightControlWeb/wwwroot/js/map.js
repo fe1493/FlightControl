d04ffee265e45f07cd9da01ae07848f3c3d7a64f
ori poria <ori.poria@live.biu.ac.il>
2020-05-18 14:12:47 +0300

    add line

--- FlightControlWeb/wwwroot/js/map.js
+++ FlightControlWeb/wwwroot/js/map.js
@@ -1,5 +1,4 @@
 ﻿
-
 function addServer() {
     const serverURL = document.getElementById("serverId");
     let myServer = {
@@ -51,70 +50,92 @@ require([
     });
 
     var graphicsLayer = new GraphicsLayer();
-        map.add(graphicsLayer);
-        myGraphicsLayer = graphicsLayer;
+    map.add(graphicsLayer);
+    myGraphicsLayer = graphicsLayer;
 
     var airplanPicture = new PictureMarkerSymbol({
         url: "https://upload.wikimedia.org/wikipedia/commons/1/1e/Airplane_silhouette.png",
         width: "50px",
         height: "50px"
     });
-        myPic = airplanPicture;
-        
-        function addPlan(a,b, id) {
-            var airplanGraphic = new Graphic();
-            airplanGraphic.attributes = {
-                name: id,
-            };
-            airplansDic[airplanGraphic.attributes.name] = airplanGraphic;
-            myGraphicsLayer.add(airplanGraphic);
-
-            var point = {
-                type: "point",
-            };
-            point.latitude = a;
-            point.longitude = b;
-
-            airplanGraphic.geometry = point;
-            airplanGraphic.symbol = myPic;
-        }
+    myPic = airplanPicture;
 
 
-        function updatePlan(a, b, name) {
-            var apg = airplansDic[name];
-            var point = {
-                type: "point",
-            };
-            point.latitude = a;
-            point.longitude = b;
-            apg.geometry = point;
-        }
+    var simpleLineSymbol = {
+        type: "simple-line",
+        color: [226, 119, 40], // orange
+        width: 2
+    };
 
-        window.addPlan = addPlan;
-        window.updatePlan = updatePlan;
-        
+    var polyline = {
+        type: "polyline",
+        paths: [
+            [32.821527826096, 34.0139576938577],
+            [32.814893761649, 34.0080602407843],
+            [33.808878330345, 34.0016642996246]
+        ]
+    };
+
+        var polylineGraphic = new Graphic({
+            geometry: polyline,
+            symbol: simpleLineSymbol
+        });
+
+    graphicsLayer.add(polylineGraphic);
+
+
+
+
+
+    function addPlan(a, b, id) {
+        var airplanGraphic = new Graphic();
+        airplanGraphic.attributes = {
+            name: id,
+        };
+        airplansDic[airplanGraphic.attributes.name] = airplanGraphic;
+        myGraphicsLayer.add(airplanGraphic);
+
+        var point = {
+            type: "point",
+        };
+        point.latitude = a;
+        point.longitude = b;
+
+        airplanGraphic.geometry = point;
+        airplanGraphic.symbol = myPic;
+    }
+
+
+    function updatePlan(a, b, name) {
+        var apg = airplansDic[name];
+        var point = {
+            type: "point",
+        };
+        point.latitude = a;
+        point.longitude = b;
+        apg.geometry = point;
+    }
+
+    window.addPlan = addPlan;
+    window.updatePlan = updatePlan;
+
 
 });
 
 function test() {
 
-    var lat= document.getElementById("latitude");
+    var lat = document.getElementById("latitude");
     var lon = document.getElementById("longitude")
     const l = Number(lat.value);
     const k = Number(lon.value);
     addPlan(l, k, "elal");
-        
-    }
+
+}
 function testUpdate() {
-    
-    var lat= document.getElementById("latitude");
+
+    var lat = document.getElementById("latitude");
     var lon = document.getElementById("longitude")
     const l = Number(lat.value);
     const k = Number(lon.value);
     updatePlan(l, k, "elal");
 }
-
-
-
-
-
