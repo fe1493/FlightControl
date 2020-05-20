dca15a4e99c35a24e00b238c00971697b770b871
yonatan gat <yonatan.gat@live.biu.ac.il>
2020-05-20 16:42:51 +0300

    jquery change

--- FlightControlWeb/wwwroot/js/main.js
+++ FlightControlWeb/wwwroot/js/main.js
@@ -9,17 +9,19 @@ function addflightplan() {
 }
 let i =0
 function getFlights() {
-    $.getJSON("../api/flight", function (data) {
-
+    var d = new Date();
+    let currentTime = d.getFullYear() + "-" + ("00" + (d.getMonth() + 1)).slice(-2) +
+        "-" + ("00" + d.getDate()).slice(-2) + "T" + ("00" + d.getHours()).slice(-2) +
+        ":" + ("00" + d.getMinutes()).slice(-2) + ":" + ("00" + d.getSeconds()).slice(-2) + "Z";
+    console.log(currentTime);
+    let productsUrl = "http://ronyut.atwebpages.com/ap2/api/Flights?relative_to=" + currentTime;
+    $.getJSON(productsUrl, function (data) {
+        console.log(data);
         data.forEach(function (flight) {
             $(flight).each(function (index, value) {
                 addFlightsTable(value);
             })
-
         })
-
-
-
     });
 }
 
