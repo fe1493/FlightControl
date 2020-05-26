fa52fdda4dada3f8bad9d92be55fc2f06f7d8e78
Efraim Paley <efraim.paley@live.biu.ac.il>
2020-05-26 12:29:51 +0300

    Allow user to input new flight plan with a file

---
+++ FlightControlWeb/wwwroot/js/dragdrop.js
@@ -0,0 +1,36 @@
+﻿let input = document.getElementById("fileInput");
+let allowedExtension = /(\.json)$/i;
+function onChange(event) {
+    let file = event.target.files[0];
+    let filePath = file.name;
+    //check the file extension is .json
+    if (!allowedExtension.exec(filePath)) {
+        alert('Please upload a .json file only');
+        $("#fileInput").val('');
+        return false;
+    }
+    //read the file and send to the function in charge of sending to the server
+    let reader = new FileReader();
+    reader.onload = (event) => {
+        // file content
+        let obj = JSON.parse(reader.result);
+        postflightplan(obj);
+    }
+    $("#fileInput").val('');
+    reader.onerror = error => reject(error);
+    reader.readAsText(file);
+}
+
+// Function that takes the new flight plan and posts it to the server
+function postflightplan(flightPlan) {
+    (async () => {
+        const rawResponse = await fetch("https://localhost:44389/api/flightplan", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(flightPlan)
+        });
+    })();
+}
